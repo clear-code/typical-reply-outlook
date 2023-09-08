f2a57a80991baa33de72afcf4eed02cd9fe3b36e
Takashi Hashida <hashida@clear-code.com>
2023-09-08 11:05:55 +0900

    Fix Config

--- Config/ConfigSerializer.cs
+++ Config/ConfigSerializer.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
 using System.IO;
+using System.Text.Json.Serialization;
 
 namespace TypicalReply.Config
 {
     internal static class ConfigSerializer<T>
     {
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) },
+            PropertyNameCaseInsensitive = true,
+        };
+
         internal static T LoadFromFile(string filePath)
         {
             if (!File.Exists(filePath))
@@ -17,19 +24,17 @@ namespace TypicalReply.Config
                 return default;
             }
             var jsonString = File.ReadAllText(filePath, Encoding.UTF8);
-            return JsonSerializer.Deserialize<T>(jsonString);
+            return JsonSerializer.Deserialize<T>(jsonString, _options);
         }
 
         internal static void SaveAsFile(T obj, string filePath)
         {
-            if (!File.Exists(filePath))
-            {
-                string directory = Path.GetDirectoryName(filePath);
-                Directory.CreateDirectory(directory);
-                File.Create(filePath);
-            }
-            var jsonString = JsonSerializer.Serialize(obj);
-            File.WriteAllText(filePath, jsonString);
+            string jsonString = JsonSerializer.Serialize(obj, _options);
+            string tmpfile = Path.GetTempFileName();
+            File.WriteAllText(tmpfile, jsonString);
+            string directory = Path.GetDirectoryName(filePath);
+            Directory.CreateDirectory(directory);
+            File.Move(tmpfile, filePath);
         }
     }
 }
--- Config/TemplateConfig.cs
+++ Config/TemplateConfig.cs
@@ -2,23 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TypicalReply.Config
 {
+    public enum ForwardType
+    {
+        Unknown = 0,
+        Attachment = 1,
+        Inline = 2,
+    }
+
+    public enum RecipientsType
+    {
+        Unknown = 0,
+        Blank = 1,
+        Sender = 2,
+        All = 3,
+        UserSpecification = 4
+    }
+
+    /// <summary>
+    /// Config for drop down item
+    /// </summary>
     public class TemplateConfig
     {
         public string Id { get; set; }
         public string Label { get; set; }
+
+        /// <summary>
+        /// Shortcut key for this item
+        /// This is not work for now.
+        /// </summary>
         public string AccessKey { get; set; }
         public string SubjectPrefix { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
         public string BodyImage { get; set; }
         public string Recipients { get; set; }
-        public string QuoteType { get; set; }
+
+        [JsonIgnore]
+        public RecipientsType RecipientsType
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.Recipients))
+                {
+                    return RecipientsType.Blank;
+                }
+                RecipientsType receipients;
+                if(Enum.TryParse(this.Recipients, true, out receipients))
+                {
+                    // If "UserSpecification" is specified, this returns Recipients.UserSpecification.
+                    // In that case, users may actually specify "UserSpecification", so this works fine.
+                    return receipients;
+                }
+                return RecipientsType.UserSpecification;
+            }
+        }
+            
+
+        public bool QuoteType { get; set; }
         public string AllowedDomains { get; set; }
         public string Icon { get; set; }
+        public ForwardType ForwardType { get; set; }
         public string Locale { get; set; }
     }
 }
