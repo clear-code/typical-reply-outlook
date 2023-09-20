59fe746cbbb54ee2db3598bd8eb25da47f829641
Takashi Hashida <hashida@clear-code.com>
2023-09-20 17:37:25 +0900

    Create package with default config

--- make.bat
+++ make.bat
@@ -8,6 +8,16 @@ msbuild /p:Configuration=Release
 @REM ==================
 iscc.exe /Odest TypicalReplyOutlook.iss
 
+@REM ==================================
+@REM Create package with default config
+@REM ==================================
+powershell -C ^
+"$version=(Get-ItemProperty bin\release\TypicalReply.dll).VersionInfo.FileVersion; ^
+ Compress-Archive ^
+   -Path dest\TypicalReplySetup-${version}.exe, DefaultConfig ^
+   -DestinationPath dest\TypicalReplyOutlook-${version}-with-Default-Config.zip ^
+   -Force"
+
 @REM ==================
 @REM Compress templates
 @REM ==================
